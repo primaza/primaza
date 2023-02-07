a992444438bf23773e58bc8e8b596ced1c67dca7
David Peraza <dperaza@redhat.com>
2023-02-07 07:23:37 -0500

    Creating or Updating Service Catalog resource
    When there is no service catalog resource and a new Registered
    Service is created, a new ServiceCatalog resource should be created.
    Also, when there is a service catalog resouce and new Registered
    Service is created, the ServiceCatalog resource should be updated.
    
    Signed-off-by: David Peraza <dperaza@redhat.com>

--- controllers/registeredservice_controller.go
+++ controllers/registeredservice_controller.go
@@ -19,7 +19,10 @@ package controllers
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -36,6 +39,9 @@ type RegisteredServiceReconciler struct {
 //+kubebuilder:rbac:groups=primaza.io,resources=registeredservices,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=primaza.io,resources=registeredservices/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=primaza.io,resources=registeredservices/finalizers,verbs=update
+//+kubebuilder:rbac:groups=primaza.io,resources=servicecatalogs,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=primaza.io,resources=servicecatalogs/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=primaza.io,resources=servicecatalogs/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -66,7 +72,6 @@ func (r *RegisteredServiceReconciler) Reconcile(ctx context.Context, req ctrl.Re
 		status = primazaiov1alpha1.RegisteredServiceStatus{
 			State: primazaiov1alpha1.RegisteredServiceStateAvailable,
 		}
-		// Add logic here to add RegisterService to catalog
 
 	} else {
 		status = primazaiov1alpha1.RegisteredServiceStatus{
@@ -81,6 +86,65 @@ func (r *RegisteredServiceReconciler) Reconcile(ctx context.Context, req ctrl.Re
 	if err != nil {
 		log.Error(err, "RegisteredService Status Failed")
 		return ctrl.Result{}, err
+	}
+
+	if status.State == primazaiov1alpha1.RegisteredServiceStateAvailable {
+		var sc primazaiov1alpha1.ServiceCatalog
+		err = r.Get(ctx, types.NamespacedName{
+			Name:      "primaza-service-catalog",
+			Namespace: rs.Namespace,
+		}, &sc)
+
+		// Extracting Keys of SED
+		sedKeys := make([]string, 0, len(rs.Spec.ServiceEndpointDefinition))
+		for i := 0; i < len(rs.Spec.ServiceEndpointDefinition); i++ {
+			sedKeys = append(sedKeys, rs.Spec.ServiceEndpointDefinition[i].Name)
+		}
+
+		// Initializing Service Catalog Service
+		scs := primazaiov1alpha1.ServiceCatalogService{
+			Name:                          rs.Name,
+			ServiceClassIdentity:          rs.Spec.ServiceClassIdentity,
+			ServiceEndpointDefinitionKeys: sedKeys,
+		}
+
+		if err != nil && errors.IsNotFound(err) {
+			log.Info("Service Catalog not found, creating new")
+			// Initializing the Service Catalog
+			serviceCatalog := primazaiov1alpha1.ServiceCatalog{
+				TypeMeta: v1.TypeMeta{
+					Kind:       "ServiceCatalog",
+					APIVersion: "v1alpha1",
+				},
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "primaza-service-catalog",
+					Namespace: rs.Namespace,
+				},
+				Spec: primazaiov1alpha1.ServiceCatalogSpec{
+					Services: []primazaiov1alpha1.ServiceCatalogService{scs},
+				},
+			}
+
+			// Create the Service Catalog
+			err = r.Create(ctx, &serviceCatalog)
+			if err != nil {
+				// Service Catalog creation failed
+				return ctrl.Result{}, err
+			}
+
+		} else if err != nil {
+			// Error that isn't due to the ServiceCatalog not found
+			return ctrl.Result{}, err
+		} else {
+			log.Info("Updating Service Catalog")
+			sc.Spec.Services = append(sc.Spec.Services, scs)
+			err = r.Update(ctx, &sc)
+			if err != nil {
+				// Service Catalog update failed
+				return ctrl.Result{}, err
+			}
+
+		}
 	}
 
 	return ctrl.Result{}, nil
--- test/acceptance/features/registerService.feature
+++ test/acceptance/features/registerService.feature
@@ -1,6 +1,6 @@
 Feature: Register a cloud service in Primaza cluster
 
-    Scenario: Cloud Service Registration, no Healthcheck provided
+    Scenario: Cloud Service Registration, no Healthcheck provided and no ServiceCatalog exists
         Given Primaza Cluster "primaza-main" is running
         When On Primaza Cluster "primaza-main", Resource is created
         """
@@ -29,4 +29,58 @@ Feature: Register a cloud service in Primaza cluster
           sla: L3
         """
         Then On Primaza Cluster "primaza-main", RegisteredService "primaza-rsdb" state will eventually move to "Available"
-        
+        And On Primaza Cluster "primaza-main", ServiceCatalog "primaza-service-catalog" will contain RegisteredService "primaza-rsdb"
+
+
+Scenario: Cloud Service Registration, no Healthcheck provided and ServiceCatalog exists
+        Given Primaza Cluster "primaza-main" is running
+        And   On Primaza Cluster "primaza-main", Resource is created
+        """
+        apiVersion: primaza.io/v1alpha1
+        kind: ServiceCatalog
+        metadata:
+          name: primaza-service-catalog
+          namespace: primaza-system
+        spec:
+          services:
+          - name: davprssql
+            serviceClassIdentity:
+            - name: type
+              value: mysqlserver
+            - name: provider
+              value: aws
+            serviceEndpointDefinitionKeys:
+            - host
+            - port
+            - user
+            - password
+            - database
+        """
+        When On Primaza Cluster "primaza-main", Resource is created
+        """
+        apiVersion: primaza.io/v1alpha1
+        kind: RegisteredService
+        metadata:
+          name: primaza-rsdb
+          namespace: primaza-system
+        spec:
+          serviceClassIdentity:
+            - name: type
+              value: psqlserver
+            - name: provider
+              value: aws
+          serviceEndpointDefinition:
+            - name: host
+              value: mydavphost.io
+            - name: port
+              value: "5432"
+            - name: user
+              value: davp
+            - name: password
+              value: quedicelagente
+            - name: database
+              value: davpdata
+          sla: L3
+        """
+        Then On Primaza Cluster "primaza-main", RegisteredService "primaza-rsdb" state will eventually move to "Available"
+        And On Primaza Cluster "primaza-main", ServiceCatalog "primaza-service-catalog" will contain RegisteredService "primaza-rsdb"
