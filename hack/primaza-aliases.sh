79ab1ecb6c5e97b3882fc0035284db88335dd9b4
Francesco Ilario <filario@redhat.com>
2023-03-02 13:34:19 +0100

    Add shell aliases for Primaza (#53)
    Add shell aliases for primaza
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>

---
+++ hack/primaza-aliases.sh
@@ -0,0 +1,75 @@
+#!/bin/env sh
+
+__primaza_get_kind_kubeconfig() {
+    kind get kubeconfig --name "$(kind get clusters | grep -E "$1\$" | fzf -1)"
+}
+
+__primaza_get_kind_cluster_noninteractive() {
+   kind get clusters | grep -E "$1\$" | head -n 1
+}
+
+__primaza_get_kind_cluster() {
+   kind get clusters | grep -E "$1\$" | fzf -1
+}
+
+__primaza_get_kind_kubeconfig_by_name()
+(
+    if kfg="$(kind get kubeconfig --name "$1")" ; then
+        printf '%s' "$kfg"
+        return 0
+    fi
+
+    return 1
+)
+
+__primaza_get_cluster_kubeconfig_path()
+(
+    kfg="/tmp/kc-primaza-$1"
+    printf '%s' "$kfg"
+)
+
+__primaza_export_env()
+(
+    # retrieve the cluster matching provided input
+    c=$(__primaza_get_kind_cluster_noninteractive "$1")
+    if [ "$c" = "" ]; then
+        printf "can not find kind cluster %s\n" "$1" >&2
+        return 1
+    fi
+
+    # retrieve kubeconfig for the cluster
+    if ! kfg="$(__primaza_get_kind_kubeconfig_by_name "$c")" ; then
+        printf "can not find kind cluster %s\n" "$c" >&2
+        return 1
+    fi
+
+    # write the kubeconfig file and print the configuration
+    kfgpath=$(__primaza_get_cluster_kubeconfig_path "$c")
+    printf '%s' "$kfg" >! "$kfgpath"
+    printf "export KUBECONFIG=%s\n" "$kfgpath"
+
+    # if the shell is not interactive inform the user on stderr
+    [ ! -t 1 ] && printf "exported context for cluster '%s' (KUBECONFIG=%s)\n" "$c" "$kfgpath" >&2
+)
+
+__primaza_kind_export_kubeconfig()
+(
+    c=$(__primaza_get_kind_cluster "$1")
+    kfg=$(__primaza_get_cluster_kubeconfig_path "$c")
+    __primaza_get_kind_kubeconfig "$c" >! "$kfg"
+)
+
+alias pek="__primaza_export_env"
+alias primaza-env="__primaza_export_env"
+alias pgc="__primaza_get_kind_cluster"
+
+# main
+alias pclo="kubectl logs -f -n primaza-system -l control-plane=controller-manager --kubeconfig=<(__primaza_get_kind_kubeconfig main)"
+alias pmgpo="kubectl get pods -n primaza-system --kubeconfig=<(__primaza_get_kind_kubeconfig main)"
+alias pmgce="kubectl get clusterenvironments -n primaza-system --kubeconfig=<(__primaza_get_kind_kubeconfig main)"
+
+# worker
+alias pwgpoall="kubectl get pods --all-namespaces --kubeconfig=<(__primaza_get_kind_kubeconfig worker)"
+alias pwgpoa="kubectl get pods -n applications --kubeconfig=<(__primaza_get_kind_kubeconfig worker)"
+alias pwgpos="kubectl get pods -n services --kubeconfig=<(__primaza_get_kind_kubeconfig worker)"
+alias pwgpo="kubectl get pods --kubeconfig=<(__primaza_get_kind_kubeconfig worker)"
