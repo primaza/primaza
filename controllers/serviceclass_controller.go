1a60746182d5f429d61785fa5cf9eabf1e6a9fe8
Francesco Ilario <filario@redhat.com>
2023-03-03 21:01:40 +0100

    Delete Service Class
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- controllers/serviceclass_controller.go
+++ controllers/serviceclass_controller.go
@@ -21,11 +21,13 @@ import (
 	"errors"
 	"fmt"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	meta "k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	primazaiov1alpha1 "github.com/primaza/primaza/api/v1alpha1"
@@ -56,11 +58,37 @@ type ServiceClassReconciler struct {
 func (r *ServiceClassReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	sc := primazaiov1alpha1.ServiceClass{}
-	if err := r.Get(ctx, req.NamespacedName, &sc, &client.GetOptions{}); err != nil {
+	sc := &primazaiov1alpha1.ServiceClass{}
+	if err := r.Get(ctx, req.NamespacedName, sc, &client.GetOptions{}); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
+	// check if instance is marked to be deleted
+	if sc.GetDeletionTimestamp() != nil {
+		if controllerutil.ContainsFinalizer(sc, clusterEnvironmentFinalizer) {
+			// run finalizer
+			if err := r.finalize(ctx, sc); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			// Remove finalizer from cluster environment
+			controllerutil.RemoveFinalizer(sc, clusterEnvironmentFinalizer)
+			if err := r.Update(ctx, sc); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+		return ctrl.Result{}, nil
+	}
+
+	// add finalizer if needed
+	if !controllerutil.ContainsFinalizer(sc, clusterEnvironmentFinalizer) {
+		controllerutil.AddFinalizer(sc, clusterEnvironmentFinalizer)
+		if err := r.Update(ctx, sc); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	// reconcile environments
 	if err := r.reconcileEnvironments(ctx, sc); err != nil {
 		return ctrl.Result{}, err
 	}
@@ -68,13 +96,11 @@ func (r *ServiceClassReconciler) Reconcile(ctx context.Context, req ctrl.Request
 	return ctrl.Result{}, nil
 }
 
-func (r *ServiceClassReconciler) reconcileEnvironments(ctx context.Context, sc primazaiov1alpha1.ServiceClass) error {
-	cee := primazaiov1alpha1.ClusterEnvironmentList{}
-	if err := r.List(ctx, &cee, &client.ListOptions{}); err != nil {
+func (r *ServiceClassReconciler) reconcileEnvironments(ctx context.Context, sc *primazaiov1alpha1.ServiceClass) error {
+	ff, err := r.getRelatedClusterEnvironments(ctx, sc.Spec.Constraints.Environments)
+	if err != nil {
 		return err
 	}
-
-	ff := r.filterClusterEnvironments(sc.Spec.Constraints.Environments, cee.Items)
 
 	errs := []error{}
 	for _, ce := range ff {
@@ -86,31 +112,8 @@ func (r *ServiceClassReconciler) reconcileEnvironments(ctx context.Context, sc p
 	return errors.Join(errs...)
 }
 
-func (r *ServiceClassReconciler) filterClusterEnvironments(
-	environmentConstraints []string,
-	clusterEnvironments []primazaiov1alpha1.ClusterEnvironment) []primazaiov1alpha1.ClusterEnvironment {
-
-	cee := []primazaiov1alpha1.ClusterEnvironment{}
-	for _, ce := range clusterEnvironments {
-		if envtag.Match(ce.Spec.EnvironmentName, environmentConstraints) {
-			cee = append(cee, ce)
-		}
-	}
-
-	return cee
-}
-
-func (r *ServiceClassReconciler) pushToServiceNamespaces(ctx context.Context, sc primazaiov1alpha1.ServiceClass, ce primazaiov1alpha1.ClusterEnvironment) error {
-	cfg, err := clustercontext.GetClusterRESTConfig(ctx, r.Client, ce.Namespace, ce.Spec.ClusterContextSecret)
-	if err != nil {
-		return err
-	}
-
-	oc := client.Options{
-		Scheme: r.Scheme,
-		Mapper: r.Mapper,
-	}
-	cli, err := client.New(cfg, oc)
+func (r *ServiceClassReconciler) pushToServiceNamespaces(ctx context.Context, sc *primazaiov1alpha1.ServiceClass, ce primazaiov1alpha1.ClusterEnvironment) error {
+	cli, err := r.prepareClient(ctx, &ce)
 	if err != nil {
 		return err
 	}
@@ -125,11 +128,110 @@ func (r *ServiceClassReconciler) pushToServiceNamespaces(ctx context.Context, sc
 		}
 
 		if err := cli.Create(ctx, sccp, &client.CreateOptions{}); err != nil {
+			if apierrors.IsAlreadyExists(err) {
+				rsc := &primazaiov1alpha1.ServiceClass{}
+				osc := client.ObjectKey{Namespace: ns, Name: sc.Name}
+				if err := cli.Get(ctx, osc, rsc, &client.GetOptions{}); err != nil {
+					return fmt.Errorf("error retrieving remote service class for update: %w ", err)
+				}
+
+				if rsc.Generation < sc.Generation {
+					rsc.Spec = sccp.Spec
+					if err := cli.Update(ctx, rsc, &client.UpdateOptions{}); err != nil {
+						return err
+					}
+				}
+				return nil
+			}
 			return err
 		}
 	}
 
 	return nil
+}
+
+func (r *ServiceClassReconciler) removeFromServiceNamespaces(ctx context.Context, sc *primazaiov1alpha1.ServiceClass, ce primazaiov1alpha1.ClusterEnvironment) error {
+	cli, err := r.prepareClient(ctx, &ce)
+	if err != nil {
+		return err
+	}
+
+	for _, ns := range ce.Spec.ServiceNamespaces {
+		sccp := &primazaiov1alpha1.ServiceClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      sc.Name,
+				Namespace: ns,
+			},
+		}
+
+		if err := cli.Delete(ctx, sccp, &client.DeleteOptions{}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *ServiceClassReconciler) removeFromEnvironments(ctx context.Context, sc *primazaiov1alpha1.ServiceClass) error {
+	ff, err := r.getRelatedClusterEnvironments(ctx, sc.Spec.Constraints.Environments)
+	if err != nil {
+		return err
+	}
+
+	errs := []error{}
+	for _, ce := range ff {
+		if err := r.removeFromServiceNamespaces(ctx, sc, ce); err != nil {
+			errs = append(errs,
+				fmt.Errorf("error deleting service class '%s' to cluster environment '%s': %w", sc.Name, ce.Name, err))
+		}
+	}
+	return errors.Join(errs...)
+}
+
+func (r *ServiceClassReconciler) finalize(ctx context.Context, sc *primazaiov1alpha1.ServiceClass) error {
+	return r.removeFromEnvironments(ctx, sc)
+}
+
+func (r *ServiceClassReconciler) prepareClient(ctx context.Context, ce *primazaiov1alpha1.ClusterEnvironment) (client.Client, error) {
+	cfg, err := clustercontext.GetClusterRESTConfig(ctx, r.Client, ce.Namespace, ce.Spec.ClusterContextSecret)
+	if err != nil {
+		return nil, err
+	}
+
+	oc := client.Options{
+		Scheme: r.Scheme,
+		Mapper: r.Mapper,
+	}
+	cli, err := client.New(cfg, oc)
+	if err != nil {
+		return nil, err
+	}
+
+	return cli, nil
+}
+
+func (r *ServiceClassReconciler) getRelatedClusterEnvironments(ctx context.Context, constraints []string) ([]primazaiov1alpha1.ClusterEnvironment, error) {
+	cee := primazaiov1alpha1.ClusterEnvironmentList{}
+	if err := r.List(ctx, &cee, &client.ListOptions{}); err != nil {
+		return nil, err
+	}
+
+	ff := r.filterClusterEnvironments(constraints, cee.Items)
+	return ff, nil
+}
+
+func (r *ServiceClassReconciler) filterClusterEnvironments(
+	environmentConstraints []string,
+	clusterEnvironments []primazaiov1alpha1.ClusterEnvironment) []primazaiov1alpha1.ClusterEnvironment {
+
+	cee := []primazaiov1alpha1.ClusterEnvironment{}
+	for _, ce := range clusterEnvironments {
+		if envtag.Match(ce.Spec.EnvironmentName, environmentConstraints) {
+			cee = append(cee, ce)
+		}
+	}
+
+	return cee
 }
 
 // SetupWithManager sets up the controller with the Manager.
