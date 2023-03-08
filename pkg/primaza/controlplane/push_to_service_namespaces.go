057bd40b8d69584e13e71640691728c313557416
Kartikey Mamgain <mamgainkartikey@gmail.com>
2023-03-08 17:58:33 +0530

    Reconcile Service Classes in service namespaces
    Signed-off-by: Kartikey Mamgain <mamgainkartikey@gmail.com>

---
+++ pkg/primaza/controlplane/push_to_service_namespaces.go
@@ -0,0 +1,53 @@
+/*
+Copyright 2023 The Primaza Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controlplane
+
+import (
+	"context"
+
+	primazaiov1alpha1 "github.com/primaza/primaza/api/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func PushServiceClassToServiceNamespaces(ctx context.Context, sc primazaiov1alpha1.ServiceClass, scheme *runtime.Scheme, controllerruntimeClient client.Client, serviceNamespaces []string, cfg *rest.Config) error {
+	oc := client.Options{
+		Scheme: scheme,
+		Mapper: controllerruntimeClient.RESTMapper(),
+	}
+	cli, err := client.New(cfg, oc)
+	if err != nil {
+		return err
+	}
+	for _, ns := range serviceNamespaces {
+		sccp := &primazaiov1alpha1.ServiceClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      sc.Name,
+				Namespace: ns,
+			},
+			Spec: sc.Spec,
+		}
+
+		if err := cli.Create(ctx, sccp, &client.CreateOptions{}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
