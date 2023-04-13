95a17147a82bca63817793c121a6a07457c633b9
Francesco Ilario <filario@redhat.com>
2023-04-13 23:40:24 +0200

    WIP App Agent
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- make/agents/app/deploy.mk
+++ make/agents/app/deploy.mk
@@ -1,6 +1,7 @@
 KUSTOMIZE_ARGS=--load-restrictor LoadRestrictionsNone
 KUSTOMIZE_OVERLAY=config/crd/overlays/agentapp
 NAMESPACE?=default
+CLUSTER_ENVIRONMENT?=default
 
 ifndef ignore-not-found
   ignore-not-found = false
@@ -17,17 +18,23 @@ uninstall: manifests kustomize ## Uninstall CRDs from the K8s cluster specified 
 .PHONY: deploy-rbac
 deploy-rbac:
 	cd config/agents/app/rbac && $(KUSTOMIZE) edit set namespace $(NAMESPACE)
-	$(KUSTOMIZE) build config/agents/app/rbac | kubectl apply -f -
+	$(KUSTOMIZE) build config/agents/app/rbac \
+		| CLUSTER_ENVIRONMENT=$(CLUSTER_ENVIRONMENT) envsubst '$$CLUSTER_ENVIRONMENT' \
+		| kubectl apply -f -
 
 .PHONY: deploy
 deploy: manifests kustomize ## Deploy controller to the K8s cluster specified in ~/.kube/config.
 	cd config/agents/app && \
 		$(KUSTOMIZE) edit set image agentapp=${IMG} && \
 		$(KUSTOMIZE) edit set namespace $(NAMESPACE)
-	$(KUSTOMIZE) build $(KUSTOMIZE_ARGS) config/agents/app | kubectl apply -f -
-	kubectl rollout status deploy/primaza-controller-agentapp -n $(NAMESPACE) -w --timeout=120s
+	$(KUSTOMIZE) build $(KUSTOMIZE_ARGS) config/agents/app \
+		| CLUSTER_ENVIRONMENT=$(CLUSTER_ENVIRONMENT) envsubst '$$CLUSTER_ENVIRONMENT' \
+		| kubectl apply -f -
+	kubectl rollout status deploy/pmz-app-$(CLUSTER_ENVIRONMENT) -n $(NAMESPACE) -w --timeout=120s
 
 .PHONY: undeploy
 undeploy: ## Undeploy controller from the K8s cluster specified in ~/.kube/config. Call with ignore-not-found=true to ignore resource not found errors during deletion.
 	cd config/agents/app && $(KUSTOMIZE) edit set namespace $(NAMESPACE)
-	$(KUSTOMIZE) build $(KUSTOMIZE_ARGS) config/agents/app | kubectl delete --ignore-not-found=$(ignore-not-found) -f -
+	$(KUSTOMIZE) build $(KUSTOMIZE_ARGS) config/agents/app \
+		| CLUSTER_ENVIRONMENT=$(CLUSTER_ENVIRONMENT) envsubst '$$CLUSTER_ENVIRONMENT' \
+		| kubectl delete --ignore-not-found=$(ignore-not-found) -f -
