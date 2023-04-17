2623e8070597b7a9591710074e71d28e70a91624
Francesco Ilario <filario@redhat.com>
2023-04-18 01:30:06 +0200

    Fix Service Catalog permissions
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- controllers/agents/app/servicecatalog_controller.go
+++ controllers/agents/app/servicecatalog_controller.go
@@ -33,10 +33,6 @@ type ServiceCatalogReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=primaza.io.primaza.io,resources=servicecatalogs,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=primaza.io.primaza.io,resources=servicecatalogs/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=primaza.io.primaza.io,resources=servicecatalogs/finalizers,verbs=update
-
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
